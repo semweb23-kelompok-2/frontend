73d77497e66b5f69bb7bb789195cbf5035036540
muhammadhaqqi <muhammad.haqqi01@ui.ac.id>
2023-11-15 16:52:44 +0700

    chore: add chakra ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Button, Text } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -29,8 +28,11 @@ export default function Home() {
               height={24}
               priority
             />
+
           </a>
         </div>
+        <Text>Chakra UI text coy</Text>
+        <Button>Chakra UI button coy</Button>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
---
+++ theme/index.ts
@@ -0,0 +1,8 @@
+import { extendTheme } from "@chakra-ui/react";
+import { fonts } from "./foundations";
+
+const overrides = {
+//   fonts: fonts,
+};
+
+export default extendTheme(overrides);
